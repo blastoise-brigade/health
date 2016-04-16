b30a98b8582726364bb4e61d636677a1f72a7721
Philly Tan <phillberttan@gmail.com>
2016-04-16 11:59:15 +0800

    Changed Name

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require './vendor/autoload.php';
 $twig = new Twig_Environment(new Twig_Loader_Filesystem('models'));
 
 echo $twig->render('page.html', array(
-  "name" => "Health by Blastoise Brigade",
+  "name" => "Presky",
   "description" => "A new Infrastructure. Enriched, Enlightened."
 ));
 
