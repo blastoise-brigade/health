0c6aa2c96f685379fb7b918a271c4a52b334ccbf
Philly Tan <phillberttan@gmail.com>
2016-05-19 12:58:32 +0800

    Added Flash in Administration Panel

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -6,7 +6,20 @@ class HomeController extends BaseController {
 
   public function showHome($request, $response, $args) {
     if (isset($_SESSION['ps_id'])) {
-      return $this->view->render($response, 'adminHome.twig');
+      $this->flash->addMessage('error', 'Your account has been suspended.');
+      $this->flash->addMessage('warning', 'We are going to be performing a maintenance update.');
+      $messages = $this->flash->getMessages();
+      if (count($messages) != 0) {
+        $messageArray = AuthController::formFlashArray($messages);
+        $renderArray = array(
+          "flash" => true,
+          "flashMessages" => $messageArray
+        );
+        return $this->view->render($response, 'adminHome.twig', $renderArray);
+      }
+      else {
+        return $this->view->render($response, 'adminHome.twig');
+      }
     }
     else {
       return $this->view->render($response, 'home.twig');
