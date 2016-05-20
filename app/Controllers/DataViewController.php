248c473eab80a72ab48595213619a1a15129b66b
Philly Tan <phillberttan@gmail.com>
2016-05-20 13:40:38 +0800

    Bug Fix on Counting Numbers

--- app/Controllers/DataViewController.php
+++ app/Controllers/DataViewController.php
@@ -7,12 +7,14 @@ class DataViewController extends BaseController {
   public function showSyncTable($request, $response, $arguments) {
     $syncTable = \App\Models\SyncModel::get();
     $arrayData = array();
+    $i = 1;
     foreach ($syncTable as $data) {
       array_push($arrayData, array(
-        "id" => $data->id,
+        "id" => $i,
         "totalvisits" => $data->totalvisits,
         "created_at" => $data->createdat
       ));
+      $i++;
     }
     return $this->view->render($response, 'adminShowSyncTableContent.twig', array(
       "tableData" => $arrayData
